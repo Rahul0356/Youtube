04e54636948d97527e7121da793993121e479d2e
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-03-11 18:37:43 +0530

    add the comment router

--- backend/routes/app.routes.js
+++ backend/routes/app.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { userController } from '../controller/user.controller';
+import { commentController } from '../controller/comment.controller';
 
 
 const router = express.Router();
@@ -10,6 +11,12 @@ router.post("/createUser", userController.createUser);
 router.get("/getUsers", userController.getUsers);
 router.put("/updateUser/:id", userController.updateUser);
 router.delete("/deleteUser/:id", userController.deleteUser);
+
+ // Comment Routes
+  app.post("/api/createComment",  authenticate, commentController.createComment);
+  app.get("/api/getComments/:id", commentController.getComments);
+  app.put("/api/editComment/:id", authenticate, commentController.editComment);
+  app.delete("/api/deleteComment/:id", commentController.deleteComment);
 
 
 export default router;
